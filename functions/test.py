c736ffce09a7e13101c5ce497fda939627e2ad90
Brian Handrigan <brian@handrigan.net>
2024-12-01 01:20:21 +0000

    core-functions coming along and loading expanded encodings

--- functions/test.py
+++ functions/test.py
@@ -1,10 +1,10 @@
 import yaml
 
-def load_config(file_path):
+def load_yaml_config(file_path):
     with open(file_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
 
-config = load_config('~/keys/project-keys/colab-settings.yaml')
+config = load_config('/home/developer/keys/project-keys/colab-settings.yaml')
 print(config)
